9e198b5dce9599b8f9793f0d4cad9dc4f74f41d3
Adisasson21 <96379294+Adisasson21@users.noreply.github.com>
2022-05-01 11:48:25 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+  name='sum_prime_numbers',
+  packages=['sum_prime_numbers'],
+  version='0.1',
+  license='MIT',
+  description='The function calculate all the prime numbers up to the number you chose',
+  author='Adi Sasson',
+  author_email='sassonadi38@gmail.com',
+  install_requires=[
+          'flask',
+          'json',
+      ],
+  python_requires='>=3.10'
+)
