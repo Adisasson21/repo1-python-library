a737262b17cba12b00ebeb2c5bd294893f1fc018
Adisasson21 <96379294+Adisasson21@users.noreply.github.com>
2022-05-08 10:30:41 +0300

    Update flaskapp.py

--- flaskApp/flaskapp.py
+++ flaskApp/flaskapp.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from sum_numbers_package.sum_prime_numbers import sumPrimer
-import json
 
 app = Flask(__name__)
 
@@ -8,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def sum_calculate():
     list_of_number = sumPrimer()
-    return json.dumps(list_of_number.calc_prime_number())
+    return str(sum(list_of_number.calc_prime_number()))
 
 
 app.run(host="0.0.0.0", port=5000, debug=True)
